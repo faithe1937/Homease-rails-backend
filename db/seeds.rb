8b0b4019330c8941515acfe651abcd2cebd78b77
Faith <faithe1937@gmail.com>
2020-01-30 09:08:35 -0600

    auth completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(firstname: 'olive', lastname: 'juice', email: 'oj@gmail.com', password: '1234')
